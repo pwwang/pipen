470bd64133ee5df6e2dded0c746033a9b86b3643
pwwang <pwwang@pwwang.com>
2018-10-26 17:54:44 -0500

    Print out tests information for testJobmgr.py

--- tests/testJobmgr.py
+++ tests/testJobmgr.py
@@ -16,65 +16,60 @@ class TestJobmgr(testly.TestCase):
 		makedirs(self.testdir)
 
 	def testJmNoJobs(self):
-		with helpers.log2str():
-			pNoJobs = Proc()
-			pNoJobs.nsub = 1
-			pNoJobs.run()
+		pNoJobs = Proc()
+		pNoJobs.nsub = 1
+		pNoJobs.run()
 
 	def testJm1(self):
-		with helpers.log2str():
-			p = Proc()
-			p.script = 'echo 123'
-			p.forks  = 1
-			p.nsub = 1
-			p.input = {'a': [1, 2]}
-			p.run()
+		p = Proc()
+		p.script = 'echo 123'
+		p.forks  = 1
+		p.nsub = 1
+		p.input = {'a': [1, 2]}
+		p.run()
 
 	def testJm2(self):
 		oPBAR_SIZE = Jobmgr.PBAR_SIZE
 		Jobmgr.PBAR_SIZE = 10
-		with helpers.log2str():
-			p1 = Proc()
-			p1.script = 'echo 123'
-			p1.forks = Jobmgr.PBAR_SIZE * 2
-			p1.nsub = 1
-			p1.input = {'a': list(range(Jobmgr.PBAR_SIZE * 2))}
-			p1.run()
+		p1 = Proc()
+		p1.script = 'echo 123'
+		p1.forks = Jobmgr.PBAR_SIZE * 2
+		p1.nsub = 1
+		p1.input = {'a': list(range(Jobmgr.PBAR_SIZE * 2))}
+		p1.run()
 		Jobmgr.PBAR_SIZE = oPBAR_SIZE
 
 	def testJm3(self):
-		with helpers.log2str():
-			p2 = Proc()
-			p2.script = '__err__ 123'
-			p2.forks = 20
-			p2.nsub = 1
-			p2.input = {'a': list(range(20))}
-			p2.errhow = 'halt'
-			try:
-				p2.run()
-			except SystemExit:
-				pass
+		p2 = Proc()
+		p2.script = '__err__ 123'
+		p2.forks = 20
+		p2.nsub = 1
+		p2.input = {'a': list(range(20))}
+		p2.errhow = 'halt'
+		try:
+			p2.run()
+		except SystemExit:
+			pass
 
 	def testJm4(self):
-		with helpers.log2str():
-			from time import time
-			p3         = Proc()
-			p3.forks   = 1
-			p3.nsub    = 1
-			p3.lang    = sys.executable
-			p3.args.i  = time()
-			p3.input   = {'a': [0]}
-			p3.errntry = 10
-			p3.errhow  = 'retry'
-			p3.script  = '''#
-			# PYPPL INDENT REMOVE
-			from time import sleep, time
-			sleep(.1)
-			t = time() - {{args.i}}
-			if t < 1:
-				exit(1)
-			'''
-			p3.run()
+		from time import time
+		p3         = Proc()
+		p3.forks   = 1
+		p3.nsub    = 1
+		p3.lang    = sys.executable
+		p3.args.i  = time()
+		p3.input   = {'a': [0]}
+		p3.errntry = 10
+		p3.errhow  = 'retry'
+		p3.script  = '''#
+		# PYPPL INDENT REMOVE
+		from time import sleep, time
+		sleep(.1)
+		t = time() - {{args.i}}
+		if t < 1:
+			exit(1)
+		'''
+		p3.run()
 
 	
 if __name__ == '__main__':
