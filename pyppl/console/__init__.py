07d4b2ac4ecb08524bd1103afbc47932644be715
pwwang <pwwang@pwwang.com>
2019-12-27 16:17:30 -0600

    Also add logger_init API in command line interface.

--- pyppl/console/__init__.py
+++ pyppl/console/__init__.py
@@ -4,6 +4,7 @@ import importlib
 from pathlib import Path
 from diot import Diot
 from pyparam import commands
+from ..logger import logger
 from ..plugin import config_plugins, pluginmgr
 from .. import __version__
 
@@ -21,4 +22,6 @@ pluginmgr.hook.cli_addcmd(commands = commands)
 def main():
 	"""Entry point"""
 	command, opts, _ = commands._parse(dict_wrapper = Diot)
+	pluginmgr.hook.logger_init(logger = logger)
+	logger.init()
 	pluginmgr.hook.cli_execcmd(command = command, opts = opts)
