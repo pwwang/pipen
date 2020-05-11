2723dce5d321309fc75b83db9efd5054f79febf7
pwwang <pwwang@pwwang.com>
2020-05-11 16:30:12 -0500

    3.2.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if os.path.exists(readme_path):
 setup(
     long_description=readme,
     name='PyPPL',
-    version='3.1.1',
+    version='3.2.0',
     description='A Python PiPeLine framework',
     python_requires='==3.*,>=3.6.0',
     project_urls={
