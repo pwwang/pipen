c68b96833de0b3e6b56115d07309c734274bffd1
pwwang <pwwang@pwwang.com>
2020-12-07 23:58:49 -0700

    Log an empty line after all processes finish

--- pipen/pipen.py
+++ pipen/pipen.py
@@ -181,6 +181,7 @@ class Pipen:
                     succeeded = False
                     break
                 proc.gc()
+            logger.info('')
             await plugin.hooks.on_complete(self, succeeded)
         # except Exception as exc:
             # logger.exception(exc)
