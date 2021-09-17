a9ed9f1ffad8143d7c0f80c002cb41241dc7a902
pwwang <pwwang@pwwang.com>
2021-09-16 23:18:29 -0700

    💬 Change end to export

--- pipen/proc.py
+++ pipen/proc.py
@@ -104,8 +104,10 @@ class Proc(ABC, metaclass=ProcMeta):
         dirsig: When checking the signature for caching, whether should we walk
             through the content of the directory? This is sometimes
             time-consuming if the directory is big.
-        end: When False, force the process not to be the end process, meaning
-            results will not be saved to the output directory of the pipeline.
+        export: When True, the results will be exported to `<pipeline.outdir>`
+            Defaults to None, meaning only end processes will export.
+            You can set it to True/False to enable or disable exporting
+            for processes
         error_strategy: How to deal with the errors
             - retry, ignore, halt
             - halt to halt the whole pipeline, no submitting new jobs
@@ -143,7 +145,7 @@ class Proc(ABC, metaclass=ProcMeta):
     args: Mapping[str, Any] = None
     cache: bool = None
     dirsig: bool = None
-    end: bool = None
+    export: bool = None
     error_strategy: str = None
     num_retries: int = None
     template: Union[str, Type[Template]] = None
@@ -172,7 +174,7 @@ class Proc(ABC, metaclass=ProcMeta):
         desc: str = None,
         args: Mapping[str, Any] = None,
         cache: bool = None,
-        end: bool = None,
+        export: bool = None,
         error_strategy: str = None,
         num_retries: int = None,
         forks: int = None,
@@ -192,8 +194,10 @@ class Proc(ABC, metaclass=ProcMeta):
             args: The arguments of the process, will overwrite parent one
                 The items that are specified will be inherited
             cache: Whether we should check the cache for the jobs
-            end: When False, force the process not to be an end process
-                if it is a terminal process
+            export: When True, the results will be exported to `<pipeline.outdir>`
+                Defaults to None, meaning only end processes will export.
+                You can set it to True/False to enable or disable exporting
+                for processes
             error_strategy: How to deal with the errors
                 - retry, ignore, halt
                 - halt to halt the whole pipeline, no submitting new jobs
@@ -247,7 +251,7 @@ class Proc(ABC, metaclass=ProcMeta):
         if submission_batch is not None:
             kwargs["submission_batch"] = submission_batch
 
-        kwargs["end"] = end
+        kwargs["export"] = export
         kwargs["input_data"] = input_data
         kwargs["requires"] = requires
         kwargs["nexts"] = None
@@ -284,8 +288,8 @@ class Proc(ABC, metaclass=ProcMeta):
         if self.desc is None:
             self.desc: str = desc_from_docstring(self.__class__)
 
-        if self.end is None:
-            self.end = bool(not self.nexts)
+        if self.export is None:
+            self.export = bool(not self.nexts)
 
         # log the basic information
         self._log_info()
@@ -614,7 +618,7 @@ class Proc(ABC, metaclass=ProcMeta):
                 "║ ║║\n"
                 "╰═┴╯\n"
             )
-            if self.end
+            if self.export
             else box.ROUNDED,
             width=min(CONSOLE_WIDTH, console_width),
         )
