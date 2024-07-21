5c88717c33fae68b64456eb5d34f89128f1b6977
pwwang <pwwang@pwwang.com>
2024-07-20 18:40:23 -0700

    test: try fixing "previous item was not torn down properly" (pytest-dev/pytest-forked#67)

--- tests/test_proc.py
+++ tests/test_proc.py
@@ -22,6 +22,84 @@ from .helpers import (
 )
 
 
+@pytest.mark.forked
+def test_more_nexts(pipen):
+    proc1 = Proc.from_proc(NormalProc)
+    Proc.from_proc(NormalProc, "proc2", requires=proc1)
+    Proc.from_proc(NormalProc, "proc3", requires=proc1)
+    ret = pipen.set_starts(proc1).run()
+    assert ret
+
+
+@pytest.mark.forked
+def test_proc_no_input(pipen):
+    with pytest.raises(ProcInputKeyError):
+        pipen.set_starts(NoInputProc).run()
+
+
+@pytest.mark.forked
+def test_unsupported_input_type(pipen):
+    with pytest.raises(ProcInputTypeError):
+        pipen.set_starts(InputTypeUnsupportedProc).run()
+
+
+@pytest.mark.forked
+def test_proc_with_input_data(pipen):
+    proc = Proc.from_proc(NormalProc, input_data=[1])
+    pipen.set_starts(proc).run()
+    assert proc.output_data.equals(pandas.DataFrame({"output": ["1"]}))
+
+
+@pytest.mark.forked
+def test_proc_with_input_callable(pipen):
+    proc = Proc.from_proc(NormalProc, input_data=[1])
+    proc2 = Proc.from_proc(
+        NormalProc, requires=proc, input_data=lambda ch: ch >> mutate(output=2)
+    )
+    pipen.set_starts(proc).run()
+    assert proc2.output_data.equals(pandas.DataFrame({"output": ["2"]}))
+
+
+@pytest.mark.forked
+def test_ignore_input_data_of_start_proc(caplog, pipen):
+    proc = Proc.from_proc(NormalProc, input_data=[1])
+    proc2 = Proc.from_proc(NormalProc, requires=proc, input_data=[2])
+    pipen.set_starts(proc).run()
+    assert "Ignoring input data" in caplog.text
+    assert proc2.output_data.equals(pandas.DataFrame({"output": ["1"]}))
+
+
+@pytest.mark.forked
+def test_proc_wasted_input_columns(caplog, pipen):
+    proc1 = Proc.from_proc(NormalProc, input_data=[1])
+    proc2 = Proc.from_proc(NormalProc, input_data=[1])
+    proc3 = Proc.from_proc(NormalProc, requires=[proc1, proc2])
+    pipen.set_starts(proc1, proc2).run()
+    assert "Wasted 1 column" in caplog.text
+
+
+@pytest.mark.forked
+def test_proc_not_enough_input_columns(caplog, pipen):
+    proc1 = Proc.from_proc(NormalProc, input_data=[1])
+    proc2 = Proc.from_proc(In2Out1Proc, requires=proc1)
+    pipen.set_starts(proc1).run()
+    assert "No data column for input: ['in2'], using None" in caplog.text
+    assert proc2.output_data.equals(pandas.DataFrame({"out": ["1_None"]}))
+
+
+@pytest.mark.forked
+def test_proc_relative_path_script(pipen):
+    pipen.set_starts(RelPathScriptProc).run()
+    script = RelPathScriptProc().script.render()
+    assert "AbCdEf" in script
+
+
+@pytest.mark.forked
+def test_script_file_exists(pipen):
+    with pytest.raises(ProcScriptFileNotFound):
+        pipen.set_starts(ScriptNotExistsProc).run()
+
+
 def test_proc_repr():
     assert repr(SimpleProc) == "<Proc:SimpleProc>"
 
@@ -74,90 +152,12 @@ def test_cached_run(caplog, pipen):
     assert caplog.text.count("Cached jobs:") == 1
 
 
-@pytest.mark.forked
-def test_more_nexts(pipen):
-    proc1 = Proc.from_proc(NormalProc)
-    Proc.from_proc(NormalProc, "proc2", requires=proc1)
-    Proc.from_proc(NormalProc, "proc3", requires=proc1)
-    ret = pipen.set_starts(proc1).run()
-    assert ret
-
-
-@pytest.mark.forked
-def test_proc_no_input(pipen):
-    with pytest.raises(ProcInputKeyError):
-        pipen.set_starts(NoInputProc).run()
-
-
-@pytest.mark.forked
-def test_unsupported_input_type(pipen):
-    with pytest.raises(ProcInputTypeError):
-        pipen.set_starts(InputTypeUnsupportedProc).run()
-
-
-@pytest.mark.forked
-def test_proc_with_input_data(pipen):
-    proc = Proc.from_proc(NormalProc, input_data=[1])
-    pipen.set_starts(proc).run()
-    assert proc.output_data.equals(pandas.DataFrame({"output": ["1"]}))
-
-
-@pytest.mark.forked
-def test_proc_with_input_callable(pipen):
-    proc = Proc.from_proc(NormalProc, input_data=[1])
-    proc2 = Proc.from_proc(
-        NormalProc, requires=proc, input_data=lambda ch: ch >> mutate(output=2)
-    )
-    pipen.set_starts(proc).run()
-    assert proc2.output_data.equals(pandas.DataFrame({"output": ["2"]}))
-
-
 def test_proc_is_singleton(pipen):
     pipen.workdir = ".pipen/"
     os.makedirs(pipen.workdir, exist_ok=True)
     p1 = SimpleProc(pipen)
     p2 = SimpleProc(pipen)
     assert p1 is p2
-
-
-@pytest.mark.forked
-def test_ignore_input_data_of_start_proc(caplog, pipen):
-    proc = Proc.from_proc(NormalProc, input_data=[1])
-    proc2 = Proc.from_proc(NormalProc, requires=proc, input_data=[2])
-    pipen.set_starts(proc).run()
-    assert "Ignoring input data" in caplog.text
-    assert proc2.output_data.equals(pandas.DataFrame({"output": ["1"]}))
-
-
-@pytest.mark.forked
-def test_proc_wasted_input_columns(caplog, pipen):
-    proc1 = Proc.from_proc(NormalProc, input_data=[1])
-    proc2 = Proc.from_proc(NormalProc, input_data=[1])
-    proc3 = Proc.from_proc(NormalProc, requires=[proc1, proc2])
-    pipen.set_starts(proc1, proc2).run()
-    assert "Wasted 1 column" in caplog.text
-
-
-@pytest.mark.forked
-def test_proc_not_enough_input_columns(caplog, pipen):
-    proc1 = Proc.from_proc(NormalProc, input_data=[1])
-    proc2 = Proc.from_proc(In2Out1Proc, requires=proc1)
-    pipen.set_starts(proc1).run()
-    assert "No data column for input: ['in2'], using None" in caplog.text
-    assert proc2.output_data.equals(pandas.DataFrame({"out": ["1_None"]}))
-
-
-@pytest.mark.forked
-def test_proc_relative_path_script(pipen):
-    pipen.set_starts(RelPathScriptProc).run()
-    script = RelPathScriptProc().script.render()
-    assert "AbCdEf" in script
-
-
-@pytest.mark.forked
-def test_script_file_exists(pipen):
-    with pytest.raises(ProcScriptFileNotFound):
-        pipen.set_starts(ScriptNotExistsProc).run()
 
 
 def test_invalid_name():
