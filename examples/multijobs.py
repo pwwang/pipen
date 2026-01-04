a4f9282edf230988ab61912312bd28bff077d6a7
pwwang <pwwang@pwwang.com>
2026-01-03 21:56:29 -0700

    chore: update multijob example  for increased parallelism and debugging

--- examples/multijobs.py
+++ examples/multijobs.py
@@ -5,14 +5,14 @@ from pipen import Proc, Pipen
 class MultiJobProc(Proc):
     """A process with multiple jobs"""
     input = "i"
-    input_data = range(10)
-    forks = 3
+    input_data = range(30)
+    forks = 8
     # Don't cache, we need to see the jobs to run every time
     cache = False
     output = "outfile:file:{{in.i}}.txt"
     # Let the job takes long the see the parallelization from the progressbar
-    script = "sleep 1; echo {{in.i}} > {{out.outfile}}"
+    script = "sleep {{job.index + 1}}; echo {{in.i}} > {{out.outfile}}"
 
 
 if __name__ == "__main__":
-    Pipen().set_starts(MultiJobProc).run()
+    Pipen(loglevel="debug").set_starts(MultiJobProc).run()
